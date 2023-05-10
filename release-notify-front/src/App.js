f11f4e6919e76d98c4070d475d5b318a77161d55
utouto97 <utouto97@gmail.com>
2023-05-10 20:59:42 +0900

    add api call

--- release-notify-front/src/App.js
+++ release-notify-front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebase-config';
 import {
@@ -7,40 +7,92 @@ import {
   signInWithRedirect,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getProducts, getSettings, postSettings } from './api';
 
 const App = () => {
-  console.log(process.env);
-  console.log(firebaseConfig);
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const [token, setToken] = useState('');
+  const [products, setProducts] = useState([]);
+  const [follows, setFollows] = useState({});
+  const [webhookUrl, setWebhookUrl] = useState('');
+
   const login = async (e) => {
     e.preventDefault();
     const provider = new GoogleAuthProvider();
     await signInWithRedirect(auth, provider);
   };
 
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      const token = await user.getIdToken(false);
-      setToken(token);
-    } else {
-      setToken('');
+  const onChangeCheckbox = (e) => {
+    setFollows({ ...follows, [e.target.value]: e.target.checked });
+  };
+
+  const save = async () => {
+    if (await postSettings(token, follows, webhookUrl)) {
+      alert('saved successfully');
     }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const t = await user.getIdToken(false);
+        setToken(t);
+      } else {
+        setToken('');
+      }
+
+      // console.log(token);
+      unsubscribe();
+    });
   });
 
-  return ( 
+  useEffect(() => {
+    (async () => {
+      if (token) {
+        setProducts(await getProducts(token));
+        const settings = await getSettings(token);
+        setFollows(settings.follows.reduce((a, v) => ({ ...a, [v]: true }), {}));
+        setWebhookUrl(settings.webhook_url);
+      }
+    })();
+  }, [token]);
+
+  return (
     <div className='mt-4 w-4/5 mx-auto'>
     <div className='w-full mt-2'>
     <h1 className='text-3xl font-bold'>Release Notify</h1>
     </div>
     <div className='w-full mt-2'>
-    { 
-      !token ? 
+    {
+      !token ?
       <div>
       <button className='px-4 py-2 border rounded hover:bg-gray-200' onClick={login}>Google Login</button>
       </div>
-      : <div>{token}</div>
+      :
+      <div>
+        <div className='mt-4'>
+          <button className='px-4 py-2 rounded-xl border-2 hover:bg-gray-200' onClick={save}>save</button>
+        </div>
+        <div className='mt-4 w-full flex justify-stretch items-center'>
+          <label className='ml-2 text-sm font-medium text-gray-900 mw-20'>webhook_url</label>
+          <input type="text" value={webhookUrl} onChange={(e) => setWebhookUrl(e.target.value)} className='ml-4 bg-gray-50 border border-gray-300 text-sm rounded-xl focus:ring-black-500 w-full p-2' />
+        </div>
+        <div className='mt-4 grid grid-cols-4'>
+          {products.map((e) => (
+            <div key={e}>
+              <input
+                type='checkbox'
+                value={e}
+                checked={follows[e] ?? false}
+                onChange={onChangeCheckbox}
+                className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+              />
+              <label className='ml-2 text-sm font-medium text-gray-900'>{e}</label>
+            </div>
+          ))}
+        </div>
+      </div>
     }
     </div>
     </div>
---
+++ release-notify-front/src/api.js
@@ -0,0 +1,50 @@
+const API_BASEURL = process.env.REACT_APP_API_BASEURL;
+
+export const getProducts = async (token) => {
+  try {
+    const res = await fetch(API_BASEURL + '/products', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+    });
+    return await res.json();
+  } catch(e) {
+    return null;
+  }
+};
+
+export const getSettings = async (token) => {
+  try {
+    const res = await fetch(API_BASEURL + '/settings', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+    });
+    return await res.json();
+  } catch(e) {
+    return null;
+  }
+};
+
+export const postSettings = async (token, follows, webhookUrl) => {
+  try {
+    const res = await fetch(API_BASEURL + '/settings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify({
+        follows: Object.keys(follows).filter((k) => follows[k]),
+        webhook_url: webhookUrl,
+      }),
+    });
+    return true;
+  } catch(e) {
+    return false;
+  }
+};
